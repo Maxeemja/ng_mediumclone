83804351e883e95bd45d2b74366ed663706ad1df
maxeemja <maxeemja.inc@gmail.com>
2024-02-25 02:38:53 +0200

    mock register action added, devtools connected

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,10 +1,14 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core'
 import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms'
+import {Store} from '@ngrx/store'
+import {register} from '../../store/actions'
+import {RegisterRequestInterface} from '../../types/registerRequest.interface'
+import {RouterLink} from '@angular/router'
 
 @Component({
   selector: 'mc-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './register.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -14,12 +18,19 @@ export class RegisterComponent {
     email: ['', Validators.required],
     password: ['', Validators.required],
   })
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private store: Store,
+  ) {}
 
   onSubmit() {
     if (!this.form.valid) {
       return alert('not valid')
     }
+    const request: RegisterRequestInterface = {
+      user: this.form.getRawValue(),
+    }
+    this.store.dispatch(register({request}))
     console.log(this.form.value)
   }
 }
